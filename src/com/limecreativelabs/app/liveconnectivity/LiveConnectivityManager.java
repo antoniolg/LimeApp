8400342349957a6510f13d697da6ad7d764febac
Antonio Leiva <antonioleivag@gmail.com>
2013-04-27 16:42:42 +0200

    Updated new tutorial: test connection live. Updated pom and some interface changes.

---
+++ src/com/limecreativelabs/app/liveconnectivity/LiveConnectivityManager.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.limecreativelabs.app.liveconnectivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Manage connectivity observers. It notifies observers when there is a connectivity status change
+ */
+public class LiveConnectivityManager {
+
+    private static List<ConnectivityObserver> mObservers;
+
+    private boolean mConnected;
+
+    private final Context mContext;
+
+    /**
+     * Unique manager in application
+     */
+    private static LiveConnectivityManager mManager;
+
+    /**
+     * Implements singleton pattern
+     *
+     * @param context Execution context
+     * @return Connectivity Manager
+     */
+    public static LiveConnectivityManager singleton(Context context) {
+
+        if (mManager == null) {
+            mManager = new LiveConnectivityManager(context);
+        }
+
+        return mManager;
+    }
+
+    /**
+     * Class constructor
+     */
+    private LiveConnectivityManager(Context context) {
+        mObservers = new ArrayList<ConnectivityObserver>();
+        mContext = context;
+        mConnected = isConnectionEnabled();
+
+    }
+
+    /**
+     * Test if connection is enabled
+     */
+    boolean isConnectionEnabled() {
+
+        try {
+            ConnectivityManager cm = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            return netInfo != null && netInfo.isConnectedOrConnecting();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * Adds observer to observers list
+     */
+    public void addObserver(ConnectivityObserver observer) {
+        mObservers.add(observer);
+        observer.manageNotification(mConnected);
+    }
+
+    /**
+     * Remove observer form observers list
+     */
+    public void removeObserver(ConnectivityObserver observer) {
+        mObservers.remove(observer);
+    }
+
+    /**
+     * Called when there's a connectivity change.
+     */
+    void notifyConnectionChange() {
+
+        mConnected = isConnectionEnabled();
+
+        for (ConnectivityObserver observer : mObservers) {
+            observer.manageNotification(mConnected);
+        }
+    }
+}
---
+++ src/com/limecreativelabs/app/liveconnectivity/LiveTestConnectionActivity.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.limecreativelabs.app.liveconnectivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import com.limecreativelabs.app.R;
+import com.limecreativelabs.app.shared.BaseActivity;
+
+public class LiveTestConnectionActivity extends BaseActivity implements ConnectivityObserver {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_livetestconnection);
+
+        LiveConnectivityManager.singleton(this).addObserver(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LiveConnectivityManager.singleton(this).addObserver(this);
+    }
+
+    //@Override
+    public void manageNotification(boolean connectionEnabled) {
+
+        TextView textView = (TextView) findViewById(R.id.text_status);
+
+        if (connectionEnabled) {
+            textView.setText(R.string.enabled);
+            textView.setBackgroundColor(getResources().getColor(R.color.holo_dark_green));
+        } else {
+            textView.setText(R.string.disabled);
+            textView.setBackgroundColor(getResources().getColor(R.color.holo_dark_red));
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LiveConnectivityManager.singleton(this).removeObserver(this);
+    }
+}
